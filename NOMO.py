ed6f1ed65e383a48b5b127fcaf23686f2929bee4
anupamtripathi <atanupamtripathi19@gmail.com>
2020-05-13 20:45:59 -0500

    Initial commit

--- NOMO.py
+++ NOMO.py
@@ -1,9 +1,8 @@
 import os
-import torch
-import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
 import numpy as np
+import cv2
+import torch
 
 
 path = "NOMO_preprocess/data"
@@ -16,9 +15,25 @@ class Nomo(Dataset):
         measurements_path = os.path.join(folder, 'NOMO-3d-400-scans_and_tc2_measurements/nomo-scans(repetitions-removed)')
         projections_path = os.path.join(folder, 'processed_data')
 
-        projections = np.empty((0, 512, 512, 3))
-        for img in os.listdir(os.path.join(projections_path, 'male')):
-            imgage = cv2.
+        for n, txt in enumerate(os.listdir(os.path.join(measurements_path, 'TC2_Female_Txt'))):
+            with open(os.path.join(measurements_path, 'TC2_Female_Txt', txt)) as f:
+                lines = f.read().strip().split('\n')[1:]
+
+        projections = []
+        for n, img in enumerate(os.listdir(os.path.join(projections_path, 'male'))):
+            image = cv2.imread(os.path.join(projections_path, 'male', img), cv2.IMREAD_GRAYSCALE)
+            projections.append(image)
+            if n == 10:
+                break
+        self.projections = np.array(projections)
+        print(np.array(projections).shape)
+
+    def __len__(self):
+        return len(self.projections)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
 
 if __name__ == "__main__":
@@ -27,5 +42,5 @@ if __name__ == "__main__":
 
     dataloader = DataLoader(transformed_dataset, batch_size=batch_size, shuffle=True)
 
-    for x in dataloader:
-        pass
+    # for x in dataloader:
+    #     pass
