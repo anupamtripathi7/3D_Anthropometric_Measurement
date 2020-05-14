91c350ea45698f22b7eda3b63ad9f3c7bb560fbb
anupamtripathi <atanupamtripathi19@gmail.com>
2020-05-13 21:41:03 -0500

    Initial commit

--- train.py
+++ train.py
@@ -11,58 +11,56 @@ from pytorch3d.loss import (
     mesh_normal_consistency,
 )
 import numpy as np
+from model import Generator, Discriminator
 import cv2
 
 
-n_iter = 10
-
-# Set the device
+batch_size = 1
+epochs = 50
+d_lr = 1e-2
+g_lr = 1e-2
+beta = 0.9
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load the SMPL mesh.
-src_mesh = os.path.join('smpl.obj')
 
-# Load the source and target images.
-src_img = cv2.imread('src.jpg')
-tar_img = cv2.imread('tar.jpg')
+def train_discriminator(d, real, generated, optimizer):
+    d.zero_grad()
 
-# We will learn to deform the source mesh by offsetting its vertices
-# The shape of the deform parameters is equal to the total number of vertices in src_mesh
-deform_verts = torch.full(src_mesh.verts_packed().shape, 0.0, device=device, requires_grad=True)
+    # train on real images
+    pred_real = d(real)
+    loss_real = loss(pred_real, torch.ones(batch_size, 1))
 
-# The optimizer
-optimizer = torch.optim.SGD([deform_verts], lr=1.0, momentum=0.9)
+    # train on generated images
+    pred_generated = d(generated)
+    loss_generated = loss(pred_generated, torch.zeros(batch_size, 1))
 
-
-# Weight for the chamfer loss
-w_chamfer = 1.0
-# Weight for mesh edge loss
-w_edge = 1.0
-# Weight for mesh normal consistency
-w_normal = 0.01
-# Weight for mesh laplacian smoothing
-w_laplacian = 0.1
+    if loss_generated + loss_real >= 0.01:
+        loss_generated.backward()
+        loss_real.backward()
+        optimizer.step()
 
 
-chamfer_losses = []
-laplacian_losses = []
-edge_losses = []
-normal_losses = []
+if __name__ == "__main__":
 
+    src_mesh = os.path.join('smpl.obj')
+    discriminator = Discriminator()
+    generator = Generator()
 
-for i in range(n_iter):
-    # Initialize optimizer
-    optimizer.zero_grad()
+    d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=d_lr)
+    g_optimizer = torch.optim.Adam(generator.parameters(), lr=g_lr)
 
-    # Deform the mesh
-    new_src_mesh = src_mesh.offset_verts(deform_verts)
+    # Load the source and target images.
+    src_img = cv2.imread('src.jpg')
+    tar_img = cv2.imread('tar.jpg')
 
-    # Weighted sum of the losses
-    loss = adversarial_loss(src_img, tar_img)
+    # We will learn to deform the source mesh by offsetting its vertices
+    # The shape of the deform parameters is equal to the total number of vertices in src_mesh
+    deform_verts = torch.full(src_mesh.verts_packed().shape, 0.0, device=device, requires_grad=True)
 
-    # Print the losses
-    print('Test Loss =  {}'.format(loss))
+    for epoch in range(epochs):
 
-    # Optimization step
-    loss.backward()
-    optimizer.step()
+        # Deform the mesh
+        new_src_mesh = src_mesh.offset_verts(deform_verts)
+
+        loss = 0
+        print('Test Loss =  {}'.format(loss))
