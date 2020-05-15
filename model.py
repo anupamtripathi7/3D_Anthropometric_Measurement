f6ac103b0bee4d112ad95db8d0790dbaf58db722
rautnikita <rautnikita77@gmail.com>
2020-05-14 22:01:37 -0500

    Initial commit

--- model.py
+++ model.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from tqdm import tqdm
+from NOMO import Nomo
+from torch.utils.data import DataLoader
+from utils import project_mesh_silhouette
+from pytorch3d.io import load_obj, save_obj, load_objs_as_meshes
+import random
 
 
 class Generator(nn.Module):
@@ -14,7 +20,7 @@ class Generator(nn.Module):
 
 class Discriminator(nn.Module):
 
-    def __init__(self, n_features):
+    def __init__(self):
         super(Discriminator, self).__init__()
         self.cnn1 = nn.Sequential(
             nn.ReflectionPad2d(1),
@@ -78,27 +84,46 @@ class ContrastiveLoss(torch.nn.Module):
 
 
 epochs = 2
+batch_size = 1
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+smpl_mesh_path = "Test/smpl_pytorch/human.obj"
+path = "NOMO_preprocess/data"
 
 model = Discriminator().cuda()
 criterion = ContrastiveLoss()
-optimizer = torch.optim.Adam(Discriminator.parameters(), lr=0.0005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 
 counter = []
 loss_history = []
 iteration_number= 0
 
+mesh_male = load_objs_as_meshes([smpl_mesh_path], device=device, load_textures=False)
+mesh_female = load_objs_as_meshes([smpl_mesh_path], device=device, load_textures=False)
+mesh = {'male': mesh_male, 'female': mesh_female}
+
+transformed_dataset = Nomo(folder=path)
+dataloader = DataLoader(transformed_dataset, batch_size=batch_size, shuffle=True)
+
 for epoch in range(epochs):
-    for i, sample in enumerate(tqdm(train_loader)):
-        img0, img1, label = data
-        img0, img1, label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
-        output1,output2 = Discriminator(img0,img1)
-        optimizer.zero_grad()
-        loss_contrastive = criterion(output1,output2,label)
-        loss_contrastive.backward()
-        optimizer.step()
-        if i % 10 == 0 :
-            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.data[0]))
-            iteration_number +=10
-            counter.append(iteration_number)
-            loss_history.append(loss_contrastive.data[0])
+    for i, sample in enumerate(tqdm(dataloader)):
+        for n, angle in enumerate([0, 90, 180, 270]):
+            projection = project_mesh_silhouette(mesh[sample['gender']], angle)
+            real_angle = angle + random.randint(-5, 5)
+            real = project_mesh_silhouette(mesh[sample['gender']], real_angle)
+            fake = sample['images'][n]
+            # img0, img1, label = data
+            # img0, img1, label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
+            output1, output2 = model(projection, real)
+            output3, output4 = model(projection, fake)
+            optimizer.zero_grad()
+            loss_contrastive_pos = criterion(output1, output2, 0)
+            loss_contrastive_neg = criterion(output3, output4, 1)
+            loss_contrastive = loss_contrastive_neg + loss_contrastive_pos
+            loss_contrastive.backward()
+            optimizer.step()
+            if i % 10 == 0 :
+                print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.data[0]))
+                iteration_number +=10
+                counter.append(iteration_number)
+                loss_history.append(loss_contrastive.data[0])
 # show_plot(counter,loss_history)
