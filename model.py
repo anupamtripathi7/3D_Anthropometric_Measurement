b48785a0445937c2e1e07eb25cb246becc47df02
rautnikita <rautnikita77@gmail.com>
2020-05-14 21:34:42 -0500

    Initial commit

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from tqdm import tqdm
 
 
 class Generator(nn.Module):
@@ -15,33 +16,89 @@ class Discriminator(nn.Module):
 
     def __init__(self, n_features):
         super(Discriminator, self).__init__()
+        self.cnn1 = nn.Sequential(
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(1, 4, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(4),
+            nn.Dropout2d(p=.2),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(4, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+            nn.Dropout2d(p=.2),
+
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(8, 8, kernel_size=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
+            nn.Dropout2d(p=.2),
+        )
+
         self.fc1 = nn.Sequential(
-            nn.Linear(n_features, 1024),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.3)
+            nn.Linear(8 * 100 * 100, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 500),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(500, 5)
         )
 
-        self.fc2 = nn.Sequential(
-            nn.Linear(1024, 512),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.3)
-        )
+    def forward_pass(self, x):
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
 
-        self.fc3 = nn.Sequential(
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.3)
-        )
+    def forward(self, input1, input2):
+        output1 = self.forward_pass(input1)
+        output2 = self.forward_pass(input2)
+        return output1, output2
 
-        self.out = nn.Sequential(
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
-        )
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.fc3(x)
-        x = self.out(x)
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
 
-        return x
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+
+        return loss_contrastive
+
+
+epochs = 2
+
+model = Discriminator().cuda()
+criterion = ContrastiveLoss()
+optimizer = torch.optim.Adam(Discriminator.parameters(), lr=0.0005)
+
+counter = []
+loss_history = []
+iteration_number= 0
+
+for epoch in range(epochs):
+    for i, sample in enumerate(tqdm(train_loader)):
+        img0, img1, label = data
+        img0, img1, label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
+        output1,output2 = Discriminator(img0,img1)
+        optimizer.zero_grad()
+        loss_contrastive = criterion(output1,output2,label)
+        loss_contrastive.backward()
+        optimizer.step()
+        if i % 10 == 0 :
+            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.data[0]))
+            iteration_number +=10
+            counter.append(iteration_number)
+            loss_history.append(loss_contrastive.data[0])
+# show_plot(counter,loss_history)
